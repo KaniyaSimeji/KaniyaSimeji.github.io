b2815151adb717e716a4532672d80805cbee5f5b
KaniyaSimeji <dev@kanium.me>
2023-05-23 23:36:04 +0900

    Update
    Adding lang property to html element gives better usability

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+const doc = () => {
+  return (
+    <Html lang="ja">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default doc;
